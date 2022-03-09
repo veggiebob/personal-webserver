3f451443f59e76911a1b1055bf0beb4c2e2632a9
veggiebob <veggiebobowman@gmail.com>
2022-03-08 20:21:18 -0500

    modify server start to require a python-config file or use 'python' as the executable

--- start-server.sh
+++ start-server.sh
@@ -20,6 +20,16 @@ else
     website_location="$1"
   fi
 
+  if [[ -f python-config ]]; then
+    . python-config
+  fi
+  
+  if [[ -z "${PYTHON_EXEC}" ]]; then
+    echo "Please provide a 'python-config' file with the environment variable
+      PYTHON_EXEC set to the name of the latest python executable"
+    echo "since it's not present, it will be set to 'python'"
+    export PYTHON_EXEC="python"
+  fi
   # start the actual server, and then continue with other tasks
   # note that SERVER_GHR_DEPS is now defined
   bash run.sh "$website_location" "$2"
