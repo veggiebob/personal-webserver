acb0061e3a201f6d0a1518f09b7082937460829e
veggiebob <veggiebobowman@gmail.com>
2022-03-09 19:59:46 -0500

    rename scripts and move arguments to config file

---
+++ run-.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+if [ $# -ne 2 ]
+then
+  echo "Needs at least 2 arguments: <website file location> <addr:port>"
+else
+  cargo build --release 2> .cargo-output
+  authbind --deep ./target/release/personal-webserver "$1" "$2"
+fi
